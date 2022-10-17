34f301e62b89befa0e4f764efe6c6707c43ea5bd
Vladimir Kurganov <err0rfrost@gmail.com>
2022-10-18 01:00:16 +0600

    feat: bin tree solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -9,38 +9,140 @@ const { NotImplementedError } = require('../extensions/index.js');
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree ? this.tree : null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+
+    function addNode(node,tree) {
+      if (node.data < tree.data) {
+        if (!tree.left)
+          tree.left = node;
+        else {
+          tree = tree.left;
+          addNode(node,tree);
+        }            
+      }
+      else if (node.data > tree.data) {
+        if (!tree.right)
+          tree.right = node;
+        else {
+          tree = tree.right;
+          addNode(node,tree);
+        }
+      }      
+    }
+
+    if (this.tree) {
+      let tree = this.tree;
+      addNode(newNode, tree);
+    }
+    else      
+      this.tree = newNode;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    function isData(tree) {
+      if (tree.data === data)
+        return true;
+      else if (tree.data > data)
+        tree = tree.left;
+      else
+        tree = tree.right;
+
+      return tree ? isData(tree):false;
+    }
+
+  if (this.tree) {
+    let tree = this.tree;
+    return isData(tree);
+    }
+    else
+      return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    function findData(tree) {
+      if (tree.data === data)
+        return tree;
+      else if (tree.data > data)
+        tree = tree.left;
+      else
+        tree = tree.right;
+
+      return tree ? findData(tree):null;
+    }
+  
+    if (this.tree) {
+      let tree = this.tree;
+      return findData(tree);
+    }
+    else
+      return false;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    function removeNode(tree, data) {
+      if (tree) {
+        if (data < tree.data) {
+          tree.left = removeNode(tree.left, data);
+          return tree;
+        }
+        else if (data > tree.data) {
+          tree.right = removeNode(tree.right, data);
+          return tree;
+        }
+        else {
+          if (!tree.left && !tree.right)
+            return null;
+          else if (!tree.left) {
+            tree = tree.right;
+            return tree;
+          }
+          else if (!tree.right) {
+            tree = tree.left;
+            return tree;
+          }
+
+          let rightNode = tree.right;
+          while (rightNode.left) {
+            rightNode = rightNode.left;
+          }
+          tree.data = rightNode.data;
+          tree.right = removeNode(tree.right, rightNode.data);
+          return tree;
+        }
+      } 
+      else 
+        return null;
+    }
+
+    this.tree = removeNode(this.tree, data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.tree) {
+      let tree = this.tree;
+      while (tree.left) {
+        tree = tree.left;
+      }
+      return tree.data;
+    }
+    else
+      return null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.tree) {
+      let tree = this.tree;
+      while (tree.right) {
+        tree = tree.right;
+      }
+      return tree.data;
+    }
+    else
+      return null;
   }
 }
 
